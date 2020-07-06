7865a672f4f4c150bbbbac572e2291790b899301
oconsuel <vlad.philippovich@list.ru>
2020-07-06 13:33:26 +0300

    add exam01

--- exam/js/index.js
+++ exam/js/index.js
@@ -1,58 +1,62 @@
+company = false;
+privilege = false;
+soc_discount = 0;
+
 function resolve1(data) {
-    let tempArr = [];
-    var selectId = document.getElementById("admArea");
+    let result = [];
+    var select = document.getElementById("admArea");
     data.forEach(elem => {
         if (elem.admArea) {
-            if (!tempArr.includes(elem.admArea)) {
-                var createOption = document.createElement("option");
-                createOption.innerHTML = elem.admArea;
-                selectId.appendChild(createOption);
-                tempArr.push(elem.admArea);
+            if (!result.includes(elem.admArea)) {
+                var opt = document.createElement("option");
+                opt.innerHTML = elem.admArea;
+                select.appendChild(opt);
+                result.push(elem.admArea);
             }
         }
     });
 }
 
 function resolve2(data) {
-    let tempArr = [];
-    var selectId = document.getElementById("district");
+    let result = [];
+    var select = document.getElementById("district");
     data.forEach(elem => {
         if (elem.district) {
-            if (!tempArr.includes(elem.district)) {
-                var createOption = document.createElement("option");
-                createOption.innerHTML = elem.district;
-                selectId.appendChild(createOption);
-                tempArr.push(elem.district);
+            if (!result.includes(elem.district)) {
+                var opt = document.createElement("option");
+                opt.innerHTML = elem.district;
+                select.appendChild(opt);
+                result.push(elem.district);
             }
         }
     });
 }
 
 function resolve3(data) {
-    let tempArr = [];
-    var selectId = document.getElementById("typeObject");
+    let result = [];
+    var select = document.getElementById("typeObject");
     data.forEach(elem => {
         if (elem.typeObject) {
-            if (!tempArr.includes(elem.typeObject)) {
-                var createOption = document.createElement("option");
-                createOption.innerHTML = elem.typeObject;
-                selectId.appendChild(createOption);
-                tempArr.push(elem.typeObject);
+            if (!result.includes(elem.typeObject)) {
+                var opt = document.createElement("option");
+                opt.innerHTML = elem.typeObject;
+                select.appendChild(opt);
+                result.push(elem.typeObject);
             }
         }
     });
 }
 
 function resolve2_1(data) {
-    let tempArr = [];
-    var selectId = document.getElementById("district1");
+    let result = [];
+    var select = document.getElementById("district1");
     data.forEach(elem => {
         if (elem.district) {
-            if (!tempArr.includes(elem.district)) {
-                var createOption = document.createElement("option");
-                createOption.innerHTML = elem.district;
-                selectId.appendChild(createOption);
-                tempArr.push(elem.district);
+            if (!result.includes(elem.district)) {
+                var opt = document.createElement("option");
+                opt.innerHTML = elem.district;
+                select.appendChild(opt);
+                result.push(elem.district);
             }
         }
     });
@@ -62,11 +66,11 @@ function selecttable(data) {
     if (data.length) {
         var cell = [];
         data.forEach(dt => {
-            cell += "<tr>";
+            cell += "<tr id='obj_"+dt.id+"'>";
             cell += "<td>"+dt.name+"</td>";
             cell += "<td>"+dt.typeObject+"</td>";
             cell += "<td>"+dt.address+"</td>";
-            
+            cell += "<td><button class='select'>Выбрать</button></td>";
         })
         
         document.getElementById("data").innerHTML = cell
@@ -74,33 +78,88 @@ function selecttable(data) {
     }
     
 }
-window.onload = function () {
-    const apilink = `http://exam-2020-1-api.std-900.ist.mospolytech.ru/api/data1`;
-    const request = new XMLHttpRequest();
-    const request2 = new XMLHttpRequest();
-    const request3 = new XMLHttpRequest();
-    const request4 = new XMLHttpRequest();
-    const request5 = new XMLHttpRequest();
-    request.open('GET', apilink);
-    request2.open('GET', apilink);
-    request3.open('GET', apilink);
-    request4.open('GET', apilink);
-    request5.open('GET', apilink);
 
-    request.onload = () => resolve1(JSON.parse(request.response));
+function count_sum() {
+    var sum = 0;
+    var count = 0;
+    document.querySelectorAll(".box").forEach(elem => {
+        let price = elem.querySelector(".priceset").innerHTML;
+        let qty = elem.querySelector(".range_value span").innerHTML;
+
+        count += parseInt(qty);
+        sum += price * qty;
+    });
+    if (company) {
+        sum *= 2.5;
+        count *= 5;
+    }
+    if (privilege && soc_discount) {
+        sum *= soc_discount;
+    }
+    document.querySelector(".final_qty").innerHTML = count;
+    document.querySelector(".final_sum").innerHTML = sum;
+}
+
+
+window.onload = function () {
+    var visible = false;
+    var id = 0;
+    var res;
+    const api = `http://exam-2020-1-api.std-900.ist.mospolytech.ru/api/data1`;
+    const request = new XMLHttpRequest();
+    request.open('GET', api);
+
+    request.onload = () => {
+        res = JSON.parse(request.response); 
+        resolve1(res);
+        resolve2(res);
+        resolve3(res);
+        selecttable(res);
+        resolve2_1(res);
+    };
     request.send();
 
-    request2.onload = () => resolve2(JSON.parse(request2.response));
-    request2.send();
+    document.querySelectorAll(".range").forEach(elem => {
+        elem.addEventListener("input", function(e) {
+            var val = e.target.parentElement.querySelector(".range_value span");
+            val.innerHTML = e.target.value;
+            count_sum();
+        });
+    });
 
-    request3.onload = () => resolve3(JSON.parse(request3.response));
-    request3.send();
+    document.addEventListener("click", function(e) {
+        if (e.target.classList.contains("select")) {
+            document.querySelectorAll(".current_res").forEach(elem => {
+                elem.classList.remove("current_res");
+            });
+            var parent = e.target.parentElement.parentElement;
+            parent.classList.add("current_res");
+            id = parent.id.split("_")[1];
 
-    request4.onload = () => selecttable(JSON.parse(request4.response));
-    request4.send();
-
-    request5.onload = () => resolve2_1(JSON.parse(request5.response));
-    request5.send();
+            var ind = 1;
+            res.forEach(r => {
+                if (r['id'] == id) {
+                    document.querySelectorAll(".box .priceset").forEach(elem => {
+                        elem.innerHTML = r['set_' + ind];
+                        ind++;
+                    });
+                    if (r['socialPrivileges']) {
+                        soc_discount = r['socialDiscount'] / 100;
+                        document.querySelector(".privilege").disabled = false;
+                        privilege = false;
+                        document.querySelector(".privilege").checked = false;
+                    } else {
+                        document.querySelector(".privilege").disabled = true;
+                    }
+                }
+            })
+            if (!visible) {
+                document.querySelector(".menu").style.display = "flex";
+                visible = true;
+            }
+            count_sum();
+        }
+    })
 
     document.querySelector("form").addEventListener("submit", function(e) {
         e.preventDefault();
@@ -113,7 +172,9 @@ window.onload = function () {
         var rests = document.querySelector("#tableRests");
         var rests_val = rests.options[rests.selectedIndex].value == "Есть" ? 1 : 0;
         document.querySelectorAll("#data tr").forEach(elem => elem.remove());
-        selecttable(JSON.parse(request.response).filter(elem => {
+        selecttable(JSON.parse(request.response).sort(function (obj1, obj2) {
+            return obj1.rate - obj2.rate;
+        }).filter(elem => {
             let result = true;
             if (area_val != "Не выбрано") {
                 result *= (elem.admArea == area_val);
@@ -128,9 +189,18 @@ window.onload = function () {
                 result *= (elem.socialPrivileges == rests_val);
             }
             return result;
-            
         }));
     });
+
+    document.querySelectorAll(".dop_opt input[type='checkbox']").forEach(elem => {
+        elem.addEventListener("change", function(e) {
+            var checked = e.target.checked;
+            if (elem.classList.contains("company")) {
+                company = checked;
+            } else {
+                privilege = checked;
+            }
+            count_sum();
+        });
+    });
 }
-
-
